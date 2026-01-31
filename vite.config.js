cf8ecfc6fd2cac283ad01d0f5d25e5bed8067e26
mernahanyk <mernahany153@gmail.com>
2026-01-31 22:08:34 +0200

    Fix vite config base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-first-project-1-31-2026/'
+  base: './'
 })
